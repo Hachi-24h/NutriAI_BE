0db2be36243f295e813cea51b084cac059178493
Hachi-24h <shinnichi987@gmail.com>
2025-11-06 22:59:37 +0700

    xong hàm chuyển hoá và lời khuyên

--- meals-services/models/scannedMeal.js
+++ meals-services/models/scannedMeal.js
@@ -14,7 +14,6 @@ const ScannedMealSchema = new mongoose.Schema({
   },
   confidence: { type: Number, default: 0 },
   scannedAt: { type: Date, default: Date.now },
-  userID: { type: String, required: true },
   mealType: { type: String, enum: ["BREAKFAST", "LUNCH", "DINNER", "OTHER"], default: "OTHER" }
 }, { timestamps: true });
 
