04637ed93fa5a11854a9e2858f608bff67cb7ca4
nguyentrongtinh <nguyentrongtinh4803@gmail.com>
2025-12-12 13:21:30 +0700

    update

--- auth-services/models/OtpCode.js
+++ auth-services/models/OtpCode.js
@@ -1,11 +1,15 @@
-// models/OtpCode.js
 const mongoose = require("mongoose");
 
 const OtpCodeSchema = new mongoose.Schema({
   email: { type: String },
   phone: { type: String },
   code: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 300 } // hết hạn 5 phút
+
+  purpose: { type: String, required: true },  // thêm mục đích
+
+  meta: { type: Object }, // dùng để lưu phone + passwordHash
+
+  createdAt: { type: Date, default: Date.now, expires: 300 }
 });
 
 module.exports = mongoose.model("OtpCode", OtpCodeSchema);
