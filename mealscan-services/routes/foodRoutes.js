00f6cd8560047fabd993c6810d8dce985f7a7381
nguyentrongtinh <nguyentrongtinh4803@gmail.com>
2025-12-17 21:01:53 +0700

    add searchfooof

--- mealscan-services/routes/foodRoutes.js
+++ mealscan-services/routes/foodRoutes.js
@@ -6,5 +6,6 @@ router.get("/search", foodController.searchFoodList);
 router.get("/detail", foodController.getFoodDetail);
 router.get("/featured", foodController.getFeaturedFoods);
 router.get("/random", foodController.getRandomFoods);
+router.post("/foods-analyze-batch", foodController.analyzeFoodsBatch);
 
 module.exports = router;
